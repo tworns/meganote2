1d1ac914a89753c0c77f51625cfe1df9f5de2f26
tworns <tworns@yahoo.com>
2016-07-10 13:00:46 -0400

    Added the actual files this time

---
+++ app/sign-up/button-directive.js
@@ -0,0 +1,40 @@
+{
+  angular.module('meganote.signup').directive('xtLogin',xtLogin);
+
+  function xtLogin(){
+    return {
+      templateUrl: 'app/sign-up/button.html',
+      restrict: 'E',
+      controller: ButtonController,
+      controllerAs: 'vm',
+      scope: {},
+    };
+  }
+  ButtonController.$inject = ['$state','AuthToken', 'CurrentUser'];
+  function ButtonController($state, AuthToken, CurrentUser){
+    var vm = this;
+    vm.logout = logout;
+    vm.login = login;
+    vm.btnTxt = 'login';
+    vm.user = CurrentUser.get();
+    if(AuthToken.get() === undefined){
+      vm.btnTxt = 'login';
+      vm.logout();
+    }
+    else {
+      vm.btnTxt = 'logout';
+      vm.login();
+    }
+
+    function logout (){
+      AuthToken.clear();
+      CurrentUser.clear();
+      $state.go('signUp');
+    }
+    function login (){
+      CurrentUser.set(vm.user);
+      AuthToken.set(vm.user);
+      $state.go('notes');
+    }
+  }
+}
